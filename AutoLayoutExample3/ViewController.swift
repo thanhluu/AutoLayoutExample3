1b0a5e995121c5275bf00334bb8af82181afb8cc
thanhluu <thanhlt90@gmail.com>
2016-06-22 22:47:51 +0700

    first commit

--- AutoLayoutExample3/ViewController.swift
+++ AutoLayoutExample3/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label1: UILabel!
+    
+    @IBOutlet weak var label2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        label1.text = "Lorem ipsum dolor sit amet"
+        label2.text = "Lorem ipsum dolor sit amet"
     }
 
     override func didReceiveMemoryWarning() {
