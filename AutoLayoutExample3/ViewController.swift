1d519b7f77f9f6cc722fcb5b84e4d6269697f380
thanhluu <thanhlt90@gmail.com>
2016-06-23 21:05:32 +0700

    Modify Layout in Code

--- AutoLayoutExample3/ViewController.swift
+++ AutoLayoutExample3/ViewController.swift
@@ -11,8 +11,8 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var label1: UILabel!
-    
     @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var labelTrailingConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         
         label1.text = "Lorem ipsum dolor sit amet"
         label2.text = "Lorem ipsum dolor sit amet"
+    }
+    
+    override func viewWillLayoutSubviews() {
+        labelTrailingConstraint.constant = 50.0
     }
 
     override func didReceiveMemoryWarning() {
